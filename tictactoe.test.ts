5cc92452dbad7d10ec830ffe3dfaca946a62a04b
Max Botwin <maxmarbot@gmail.com>
2021-10-22 16:25:05 -0500

    added describe block

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -19,15 +19,18 @@ test('I can start a game', async () => {
     
 });
 
-test('I can click the first square', async () => {
-    let square0 = driver.findElement(By.id('cell-0'))
-    await square0.click()
-})
-test('I can click the first square', async () => {
-    let square2 = driver.findElement(By.id('cell-2'))
-    await square2.click()
-})
-test('I can click the first square', async () => {
-    let square6 = driver.findElement(By.id('cell-6'))
-    await square6.click()
+describe('Adding Xs to the squares', async () => {
+    
+    test('I can click the first square', async () => {
+        let square0 = driver.findElement(By.id('cell-0'))
+        await square0.click()
+    })
+    test('I can click the first square', async () => {
+        let square2 = driver.findElement(By.id('cell-2'))
+        await square2.click()
+    })
+    test('I can click the first square', async () => {
+        let square6 = driver.findElement(By.id('cell-6'))
+        await square6.click()
+    })
 })
